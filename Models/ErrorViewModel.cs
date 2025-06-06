f93fa111860b8d1024bab7a1f3e493d5ff65cad3
JoseARMANDORosa <josephalexandergarciagonzalez.gen23@cbtis105.edu.mx>
2025-06-06 09:35:39 -0600

    Ya casi terminamos

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Configuracion_de_Proyecto.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/MiProyecto.cs
@@ -0,0 +1,8 @@
+namespace Configuracion_de_Proyecto.Models
+{
+    public class MiProyecto
+    {
+        public string NombreIntegrante1 { get; set; } = "Joseph Alexander Garcia Gonzalez";
+        public string NombreIntegrante2 { get; set; } = "Jose Armando Rosalino Cristobal";
+    }
+}
